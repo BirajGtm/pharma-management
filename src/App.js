0732fb975b3bd7e0d34fe4b52a4959b7863e23ec
Biraj Gautam <biraj@techstacksolutions.com>
2019-05-15 22:05:46 +0545

    cra w/ semantic ui setup completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Container, Header } from "semantic-ui-react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <br />
+        <Container>
+          <Header as="h2">Hello</Header>
+          <p>This is Pharma app Homepage created using semantic-ui.</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+            commodo ligula eget dolor. Aenean massa strong. Cum sociis natoque
+            penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+            Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
+            Nulla consequat massa quis enim. Donec pede justo, fringilla vel,
+            aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut,
+            imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede
+            link mollis pretium. Integer tincidunt. Cras dapibus. Vivamus
+            elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo
+            ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam
+            lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus
+            viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean
+            imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper
+            ultricies nisi.
+          </p>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default App;
